be3f0f6fd1a169fc0454f03e0ecd6cc80a7695fc
ChasonJia <chasonjia.dev@gmail.com>
2026-02-20 22:23:51 -0600

    feat: add token

--- routers/userVacRoutes.js
+++ routers/userVacRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getUserVacs } from "../controllers/userVacController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/history", getUserVacs);
+router.get("/history", authMiddleware, getUserVacs);
 
 export const userVacRoutes = router;
